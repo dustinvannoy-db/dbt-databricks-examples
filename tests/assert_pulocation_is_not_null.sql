29d055aca7b8051eb928445dc3b120447fa9796c
dustinvannoy-db <dustin.vannoy@databricks.com>
2023-08-16 10:02:42 -0700

    Fix broken dbt tests

--- tests/assert_pulocation_is_not_null.sql
+++
@@ -1,6 +0,0 @@
-{{ config(store_failures = true) }}
-
--- notes: quarantine records and isolate them if the total  amount is negative -- 
-select *
-from {{ref('dbt_silver_nyctaxi_trip')}}
-where PULocationID is null
--- tests/assert_vendor_fare_amount_must_be_above_zero.sql
+++ tests/assert_vendor_fare_amount_must_be_above_zero.sql
@@ -1,6 +1,5 @@
 {{ config(store_failures = true) }}
 
--- notes: quarantine records and isolate them if the total sales amount is negative -- 
 select VendorId, sum(fare_amount) TotalFareAmount
 from {{ref('dbt_silver_nyctaxi_trip')}}
 group by VendorId 
